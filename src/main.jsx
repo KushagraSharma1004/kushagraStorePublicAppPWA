7b103c40882661dcb3b845ba8a4f2e78f08c9180
KushagraSharma1004 <161684316+KushagraSharma1004@users.noreply.github.com>
2024-12-29 13:01:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import App from './App.jsx';
-import { Routes, Route } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/kushagraStorePublicAppPWA/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
